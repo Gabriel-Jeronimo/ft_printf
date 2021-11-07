5236100aadb935c36256feccf64c5b52a95d770b
Gabriel-Jeronimo <gabrieljeronimo-@hotmail.com>
2021-11-07 14:48:27 +0100

    feat: print nill if pointer equals 0

--- print_pointer.c
+++ print_pointer.c
@@ -6,7 +6,7 @@
 /*   By: gjeronim <gjeronim@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 22:42:00 by coder             #+#    #+#             */
-/*   Updated: 2021/11/07 01:58:21 by gjeronim         ###   ########.fr       */
+/*   Updated: 2021/11/07 14:36:21 by gjeronim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	print_pointer(unsigned long int num)
 {	
+	if (!num)
+		return (write(1, "(nil)", 5));
 	write(1, "0x", 2);
 	return (convert_to_hexadecimal(num, "0123456789abcdef") + 2);
 }
