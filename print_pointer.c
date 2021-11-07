1687adbb3ee71879f6b6a54094e7abafbdf9cc65
Gabriel-Jeronimo <gabrieljeronimo-@hotmail.com>
2021-11-07 15:33:32 +0100

    refactor: Change nill pointer rule

--- print_pointer.c
+++ print_pointer.c
@@ -6,7 +6,7 @@
 /*   By: gjeronim <gjeronim@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 22:42:00 by coder             #+#    #+#             */
-/*   Updated: 2021/11/07 14:36:21 by gjeronim         ###   ########.fr       */
+/*   Updated: 2021/11/07 15:27:46 by gjeronim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	print_pointer(unsigned long int num)
 {	
-	if (!num && IS_MACOS)
+	if (!num && !IS_MACOS)
 		return (write(1, "(nil)", 5));
 	write(1, "0x", 2);
 	return (convert_to_hexadecimal(num, "0123456789abcdef") + 2);
